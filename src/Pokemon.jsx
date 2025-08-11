6950e2698c80fa38dd620362ddadfc7e1a2db5d7
unknown <kshitizmukunde6@gmail.com>
2025-08-11 08:38:41 +0530

    App Completed

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -7,6 +7,7 @@ export const Pokemon = () => {
     const [pokemon, setPokemon] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [search, setSearch] = useState("");
 
     const API = "https://pokeapi.co/api/v2/pokemon?limit=24";
 
@@ -14,7 +15,7 @@ export const Pokemon = () => {
         try {
             const res = await fetch(API);
             const data = await res.json();
-            // console.log(data);
+            console.log(data);
 
             // const detailedPokemonData = data.results.map((curPokemon) => {
             //     console.log(curPokemon.url);
@@ -41,7 +42,14 @@ export const Pokemon = () => {
 
     useEffect(() => {
         fetchPokemon();
-    }, [])
+    }, []);
+
+
+    // Search Functionality
+
+    const searchData = pokemon.filter((curPokemon) => 
+        curPokemon.name.toLowerCase().includes(search.toLowerCase())
+    );
 
     if(loading){
         return (
@@ -66,9 +74,13 @@ export const Pokemon = () => {
                 <header>
                     <h1> Lets Catch Pokemon </h1>
                 </header>
+                <div className="pokemon-search">
+                    <input type="text" placeholder="Search Pokemon" value={search} onChange={(e) => setSearch(e.target.value) }/> 
+                </div>
                 <div>
                     <ul className="cards">
-                        {pokemon.map((curPokemon) => {
+                        {/* {pokemon.map((curPokemon) => { */}
+                        {searchData.map((curPokemon) => {
                             return (
                                 <PokemonCards key={curPokemon.id} pokemonData = {curPokemon} />
                             )
